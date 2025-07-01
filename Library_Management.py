7f353db76162827a63ee2c2d934d7e9a473ebfd5
samskull <samskull>
2025-07-01 14:57:50 +0600

    Book Class Created

--- Library_Management.py
+++ Library_Management.py
@@ -1,14 +1,25 @@
+class Book:
+    def __init__(self, book_id, book_name):
+        self.book_id = book_id
+        self.book_name = book_name
+
+    def __repr__(self):
+        return f"Book ID : {self.book_id} | Book Title : {self.book_name}"
+   
 class Library:
     book_list = []
 
-    def entry_book(self, book_id, book_name):
-        self.book_id = book_id
-        self.book_name = book_name
-        self.book_list.append([self.book_id, self.book_name])
+    def entry_book(self, book):
+        self.book_list.append(book)
 
 
 books = Library()
+book1 = Book(1, "Prince")
+book2 = Book(2, "Fault")
 
-books.entry_book(121, 'Harry Potter')
-books.entry_book(174, 'Dune')
+books.entry_book(book1)
+books.entry_book(book2)
+
+print(book1)
+print(book2)
 print(books.book_list)
